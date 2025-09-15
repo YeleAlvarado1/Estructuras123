be62740d669a19a56673d6bc9ef323098555cdd7
Tu Nombre <tu_correo@example.com>
2025-09-14 19:47:02 -0500

    Agregar inicialización del arreglo de revistas en la clase Catalogo

--- Semana13/Catalogo.cs
+++ Semana13/Catalogo.cs
@@ -1,5 +1,20 @@
 public class Catalogo
 {
     Revista[] revistas = new Revista[10];
+
+    public Catalogo()
+    {
+    revistas[0] = new Revista("National Geographic"); // Inicialización del arreglo de revistas
+    revistas[1] = new Revista("Generación 21");
+    revistas[2] = new Revista("Ciencia Hoy");
+    revistas[3] = new Revista("Acelerando");
+    revistas[4] = new Revista("Historia y Vida");
+    revistas[5] = new Revista("Cosas");
+    revistas[6] = new Revista("Revista Hogar");
+    revistas[7] = new Revista("Revista vistazo");
+    revistas[8] = new Revista("Discover Magazine");
+    revistas[9] = new Revista("Revista estadio");
+    }
+
 }
     
