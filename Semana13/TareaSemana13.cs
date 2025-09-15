7410f0a56a9abbffad112db4566c60182d2e339a
Tu Nombre <tu_correo@example.com>
2025-09-14 20:07:05 -0500

    Refactorizar el bucle del menú en TareaSemana13 para mejorar la legibilidad y la estructura del código. Se realizaron pruebas y todo Ok

--- Semana13/TareaSemana13.cs
+++ Semana13/TareaSemana13.cs
@@ -16,16 +16,43 @@ public class TareaSemana13
             System.Console.WriteLine("3. Salir");
             System.Console.Write("Seleccione una opcion: ");
             opcion = System.Console.ReadLine();
-        if (opcion == "1")
-        {
-            catalogo.Mostrar(); // Llama al metodo que imprime las revistas
-        }
-        else if (opcion == "2")
-        {
-            System.Console.Write("Ingrese el título a buscar: ");
-            entrada = System.Console.ReadLine();
-        }
-            
+            if (opcion == "1")
+            {
+                catalogo.Mostrar(); // Llama al metodo que imprime las revistas
+            }
+            else if (opcion == "2")
+            {
+                System.Console.Write("Ingrese el título a buscar: ");
+                entrada = System.Console.ReadLine();
+                // Validar que la entrada no este vacia
+                if (entrada != null && entrada != "")
+                {
+                    bool encontrado = catalogo.BuscarRecursivo(entrada, 0);
 
+                    if (encontrado)
+                    {
+                        System.Console.WriteLine("Encontrado");
+
+                    }
+                    else
+                    {
+                        System.Console.WriteLine("No encontrado");
+                    }
+                }
+                else
+                {
+                    System.Console.WriteLine("Debe ingresar un título válido.");
+                }
+            }
+            else if (opcion == "3")
+            {
+                System.Console.WriteLine("Saliendo del programa...");
+            }
+            else
+            {
+                System.Console.WriteLine("Opción no válida. Intente nuevamente.");// Mensaje de error para opcion invalida
+            }
+        }
     }
 }
+
